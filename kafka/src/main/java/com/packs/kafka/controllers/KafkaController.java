14af6e610cc2f8ca7fab025f9e577f0ae9c2f6cc
Vasanth Muralikrishnan <42834098+mrmvasanth@users.noreply.github.com>
2019-05-19 18:41:42 +0530

    Thread implemented for producer

--- kafka/src/main/java/com/packs/kafka/controllers/KafkaController.java
+++ kafka/src/main/java/com/packs/kafka/controllers/KafkaController.java
@@ -1,18 +1,16 @@
 package com.packs.kafka.controllers;
 
-
-
 import com.packs.kafka.services.Producer;
+import com.packs.kafka.services.ProducerThread;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/kafka")
 public class KafkaController {
     private final Producer producer;
+
+    private static final String TOPIC = "topic1";
 
     @Autowired
     public KafkaController(Producer producer) {
@@ -20,7 +18,18 @@ public class KafkaController {
     }
 
     @PostMapping(value = "/publish")
-    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+    public String sendMessageToKafkaTopic(@RequestParam("message") String message) {
         this.producer.sendMessage(message);
+        return "published successfully";
     }
+
+    @PostMapping(value = "/startserver")
+    public String startServer(){
+        boolean isAsync = false;
+        ProducerThread producerThread = new ProducerThread(TOPIC, isAsync);
+        producerThread.start();
+        return "Server Started";
+    }
+
+
 }
--- kafka/src/main/java/com/packs/kafka/services/Producer.java
+++ kafka/src/main/java/com/packs/kafka/services/Producer.java
@@ -1,5 +1,6 @@
 package com.packs.kafka.services;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +8,17 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Producer {
+public class Producer{
     private static final Logger logger = LoggerFactory.getLogger(Producer.class);
-    private static final String TOPIC = "users";
+    private static final String TOPIC = "topic1";
+
     @Autowired
-    private KafkaTemplate<String,String> kafkaTemplate;
-    public void sendMessage(String message){
-        logger.info(String.format("$$ -> Producing message --> %s",message));
-        this.kafkaTemplate.send(TOPIC,message);
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        logger.info(String.format("$$ -> Producing message --> %s", message));
+        this.kafkaTemplate.send(TOPIC, message);
+
     }
+
 }
