23ad27918f685b02a81038d6c37af17dd4a3ddd8
mrmvasanth <vasanthmuraikrishnan@gmail.com>
2019-05-24 18:14:16 +0530

    Basic flow is done with maximum corrections

--- kafka/src/main/java/com/packs/kafka/KafkaApplication.java
+++ kafka/src/main/java/com/packs/kafka/KafkaApplication.java
@@ -1,7 +1,5 @@
 package com.packs.kafka;
 
-import com.packs.kafka.repository.CassandraRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
